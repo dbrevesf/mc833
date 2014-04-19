daa01a87ee1b69f1fcdcc5740d2ea51420f117e9
Daniel Breves Ferreira <dbrevesf@gmail.com>
2014-04-19 18:42:26 -0300

    Adicao de categorias no DB

--- populate.py
+++ populate.py
@@ -13,19 +13,20 @@ cursor.execute("""CREATE TABLE estabelecimentos(
 					coord_x REAL NOT NULL,
 					coord_y REAL NOT NULL,
 					information TEXT,
+                    category TEXT,
 					rate REAL)""")
 print("+ database created")
 
-estabelecimentos = [(001, 'Posto Shell', 0, 300, 'Posto de gasolina e conveniencia', 5),
-					(002, 'McDonalds', -50, 300, 'Fast-food gorduroso', 7),
-					(003, 'BarDoZe', -200, 300, 'Melhor bar de Barao', 10),
-					(004, 'BurgerKing', -600, 300, 'Fast-food mais gorduroso', 5),
-					(005, 'Dogao', -250, 350, 'Melhor hot-dog do mundo', 8),
-					(006, 'Subway', 100, 300, 'Fast-food saudavel', 6),
-					(007, 'Eskina', -200, 350, 'Bar honesto', 7)]
+estabelecimentos = [(001, 'Posto Shell', 0, 300, 'Posto de gasolina e conveniencia', 'conveniencia', 5),
+					(002, 'McDonalds', -50, 300, 'Fast-food gorduroso', 'fastfood', 7),
+					(003, 'BarDoZe', -200, 300, 'Melhor bar de Barao', 'bar', 10),
+					(004, 'BurgerKing', -600, 300, 'Fast-food mais gorduroso', 'fastfood', 5),
+					(005, 'Dogao', -250, 350, 'Melhor hot-dog do mundo','fastfood', 8),
+					(006, 'Subway', 100, 300, 'Fast-food saudavel', 'fastfood', 6),
+					(007, 'Eskina', -200, 350, 'Bar honesto', 'bar', 7)]
 
 
-cursor.executemany("INSERT INTO estabelecimentos VALUES (?,?,?,?,?,?)", estabelecimentos)
+cursor.executemany("INSERT INTO estabelecimentos VALUES (?,?,?,?,?,?,?)", estabelecimentos)
 
 print("+ database populated")
 
