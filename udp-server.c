80b6dab926dd9e7320e04dde72c3303e7af112c4
Daniel Breves Ferreira <dbrevesf@gmail.com>
2014-04-18 22:37:29 -0300

    Get user position

--- udp-server.c
+++ udp-server.c
@@ -11,63 +11,80 @@ char** split(char* string, char delimiter);
 
 int main(int argc, char**argv)
 {
-   int sockfd,n;
-   struct sockaddr_in servaddr,cliaddr;
-   socklen_t len;
-   char mesg[1000];
+    int sockfd,n, i;
+    struct sockaddr_in servaddr,cliaddr;
+    socklen_t len;
+    char mesg[1000];
 
-   sockfd=socket(AF_INET,SOCK_DGRAM,0);
+    typedef struct{
+        char *x;
+        char *y;
+    }Posicao; 
+    Posicao posicao;
+    sockfd=socket(AF_INET,SOCK_DGRAM,0);
 
-   bzero(&servaddr,sizeof(servaddr));
-   servaddr.sin_family = AF_INET;
-   servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-   servaddr.sin_port=htons(32000);
-   bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    servaddr.sin_port=htons(32000);
+    bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
 
-   for (;;)
-   {
-      len = sizeof(cliaddr);
-      n = recvfrom(sockfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
-      sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
-      
+    for (;;)
+    {
+        len = sizeof(cliaddr);
+        n = recvfrom(sockfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
+        sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
 
-      printf("TAMNHO DA STRING: %ld", sizeof(mesg));
-
-
-      printf("Received the following:\n");
-      char** strings = split(mesg, ',');
-      printf("-------------------------------------------------------\n");
-      
-   
+                
+        char** entrada = split(mesg, ':');
+        if (strcmp(entrada[0], "posicao") == 0){
+            char** posicoes = split(entrada[1], ',');      
+            posicao.x = (char*)malloc(strlen(posicoes[0])*sizeof(char));
+            posicao.y = (char*)malloc(strlen(posicoes[1])*sizeof(char));    
+            strcpy(posicao.x, posicoes[0]);
+            strcpy(posicao.y, posicoes[1]);
+        }
+            
    }
 
 }
 
-char** split(char* message, char delimiter){
 
-   int i = 0, numeroStrings = 0;
-   char* auxString = NULL;
-   char** response = NULL;
-   
-   for(i=0; i<sizeof(message); i++){
+char **split(char frase[], char separador)
+{
+    int i, j, k, contsep = 0;
+ 
+    for(i=0,contsep=0;i<strlen(frase);i++)
+    if(frase[i] == separador)
+        contsep++;
+ 
+    char  aux[contsep][20];
+    char **result = (char**)malloc(contsep*sizeof(char));
      
-      printf("\n%d %c\n", i, message[i]);
-     /* if (string[i] != delimiter || string[i] != '\n'){
-         auxString = (char*)realloc(auxString, (i+1)*sizeof(char));         
-         auxString[i] = string[i];
-      }
-      else{
-         numeroStrings++;
-         i++;
-         int j = 0;
-         response = (char**)realloc(response, numeroStrings*sizeof(char*));
-         response[numeroStrings] = (char*)malloc(sizeof(auxString));
-         strcpy(response[numeroStrings], auxString);
-      }*/
-   }
-
-   printf("SAI FORA");
-   return response;
-
+    if(contsep)
+    {
+        for(i=0; i<=contsep; i++ )
+            *(result + i) = (char*)malloc(40*sizeof(char));
+         
+        for(i=0,k=0,j=0; i < strlen(frase); i++)
+        if(frase[i] != separador)
+        {
+            aux[k][j] = frase[i];
+            j++;
+        }
+        else
+        {
+            aux[k][j] = 0;
+            k++;
+            j=0;
+        }
+        aux[k][j] = 0;
+         
+        for(i=0;i<=contsep;i++)
+            *(result+i) = strcpy(*(result+i), aux[i]);
+     
+        return result;
+    }
+    else
+        printf("Nenhum Separador Encontrado");
 }
-
